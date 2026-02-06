88f9919b9768c10838927f1760504633ee8ce88a
Eric Munoz <128002939+Ericolink@users.noreply.github.com>
2026-02-06 16:05:47 -0700

    Se valido contenido del JSON y se evitan fallos en la carga

--- storage.py
+++ storage.py
@@ -7,8 +7,24 @@ def load_tasks():
     if not os.path.exists(FILE_NAME):
         return []
 
-    with open(FILE_NAME, "r", encoding="utf-8") as file:
-        return json.load(file)
+    try:
+        with open(FILE_NAME, "r", encoding="utf-8") as file:
+            data = json.load(file)
+
+            # Validar que el contenido sea una lista
+            if isinstance(data, list):
+                return data
+            else:
+                print("Error: El archivo JSON no tiene el formato esperado (lista de tareas).")
+                return []
+
+    except json.JSONDecodeError:
+        print("Error: El archivo JSON está corrupto o mal formado.")
+        return []
+
+    except Exception as e:
+        print(f"Error inesperado al cargar tareas: {e}")
+        return []
 
 
 def save_tasks(tasks):
